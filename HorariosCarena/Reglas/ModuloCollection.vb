3131b2a62647922667ed237ce73366e7fc4b0617
Carlos Murúa <carlosmurua@gmail.com>
2015-10-03 12:04:34 -0300

    Delete ModuloCollection.vb

--- HorariosCarena/Reglas/ModuloCollection.vb
+++
@@ -1,8 +0,0 @@
-﻿Imports System.ComponentModel
-Public Class ModuloCollection
-    Inherits BindingList(Of ModuloClass)
-
-    Protected Overrides Sub OnAddingNew(ByVal e As AddingNewEventArgs)
-        e.NewObject = New ModuloClass
-    End Sub
-End Class

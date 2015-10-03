71c81266032f3b07c73cd3751990b4ac5b659e72
Carlos Murúa <carlosmurua@gmail.com>
2015-10-03 12:06:09 -0300

    Delete AsignaturasCollection.vb

--- HorariosCarena/Reglas/AsignaturasCollection.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System.ComponentModel
-Public Class AsignaturasCollection
-
-    Inherits BindingList(Of AsignaturaClass)
-    Protected Overrides Sub OnAddingNew(ByVal e As AddingNewEventArgs)
-        e.NewObject = New AsignaturaClass  'DocenteCollection ?
-    End Sub
-
-End Class
